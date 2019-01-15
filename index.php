a396bfde07700cbe5a2f25d743adb04a36ab4250
mantasms <m.sakaitis@gmail.com>
2019-01-15 11:24:21 +0200

    failai sukurti

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+use Keygen\Keygen;
+
+?>
+<html>
+    <head>
+    </head>
+    <body>
+        <img src='images/bg.jpg';
+    </body>
+</html>
