68b2dd9ceca5cce08174ea87af071161274d0ca4
mantasms <m.sakaitis@gmail.com>
2019-01-16 10:04:25 +0200

    finish

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require 'vendor/autoload.php';
+
+use Strugleris\Strug;
+
+Strug::bla();
---
+++ vendor/phpmantasms/strugleris/tests/Strugleris/Strug.php
@@ -0,0 +1,7 @@
+<?php
+namespace Strugleris;
+class Strug {
+	public static function bla() {
+		echo "testine versija v1.0";
+	}
+}
